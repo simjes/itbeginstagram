5d4a8a0c7d385485c0ad57d4569a41190c417262
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2022-05-15 19:48:51 +0200

    Initial Commit

---
+++ instaclone/instacloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instacloneApp.swift
+//  instaclone
+//
+//  Created by Simon Jespersen on 15/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct instacloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
